e30dc0e609cc05ad4f8aedf42bde91f2569d9c63
Enzo Seuve <enzo.seuve@gmail.com>
2020-08-24 09:27:38 +0200

    Ajout de la fonctionnalité mettre en pause le chrono

--- src/components/Chronometre.js
+++ src/components/Chronometre.js
@@ -24,46 +24,41 @@ class Chronometre extends React.Component {
   // };
 
   modificationTimer() {
-    // console.log('interval', this.state.compteurBase)
-    setInterval(
-       ()  => {
+    this.interval = setInterval(
+      () => {
         this.setState({ compteurBase: this.state.compteurBase + 1 });
       }
-      ,1000)
-    };
+      , 1000)
+  };
 
-    pauseTimer() {
-      console.log('pauseeee', this.state.pauseTimer);
-      clearInterval({compteurBase: this.state.compteurBase})
-      //   () => {
-      //     this.setState({ compteurBase: this.state.compteurBase });
-      //   }
-      // )
-      };
-// this.state({compteurBase: this.state.compteurBase })
+  pauseTimer() {
+    console.log("pauseTimer", this.pauseTimer)
+    clearInterval(this.interval)
+  };
+  // this.state({compteurBase: this.state.compteurBase })
   render() {
-
-      console.log('PAUSE', this.pauseTimer);
-      return(
-      <div className = "App" >
-          <h2>{this.state.compteurBase}</h2>
-          <button onClick={event => {
-            {
-              this.pauseTimer();
-            }
-          }}>Pause</button>
-          <button onClick={event => {
-            {
-              this.modificationTimer();
-            }
-          }}>++</button>
-        {/* <Affichage affichageDuChrono={this.state.compteurBase} /> */ }
+    return (
+      <div className="App" >
+        <h2>{this.state.compteurBase}</h2>
+        <button onClick={event => {
+          {
+            this.pauseTimer();
+          }
+        }}>Pause</button>
+        <button onClick={event => {
+          {
+            this.modificationTimer();
+          }
+        }}>++</button>
+        {/* <Affichage affichageDuChrono={this.state.compteurBase} /> */}
         {/* <FonctionnaliteDuChrono fonctionnaliteDuChrono={this.state.compteurBase} fonctionnaliteDuChronoPlusUn={() => this.modificationDuStatePlusUn()}
         fonctionnaliteDuChronoMoinsUn={() => this.modificationDuStateMoinsUn()} fonctionnaliteDuChronoRemiseAZero={() => this.modificationDuStateRemiseAZero()} /> */}
-        
+
       </div>
     );
   }
 }
 
 export default Chronometre;
+
+// {compteurBase: this.state.compteurBase })
