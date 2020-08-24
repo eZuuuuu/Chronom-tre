c7234a23ee021626d16df4d833600a7ada04bd88
Enzo Seuve <enzo.seuve@gmail.com>
2020-08-24 15:56:59 +0200

    ajout de la fonctionnalité qui permet de supprimer une tache ajoutée

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -22,16 +22,26 @@ class ToDoList extends React.Component {
     this.setState({ taches: [...this.state.taches, this.state.valeurChamp] })
   }
 
+  supprimerUnetache(element) {
+    this.setState({
+      taches: [...this.state.taches.slice(0, element), ...this.state.taches.slice(element + 1)]
+    });
+
+  }
 
   render() {
     console.log('pwett', this.state)
     return (
       <div className="App" >
         <input type="text" value={this.state.valeurChamp} onChange={this.ecrireUneTache} />
-        <button onClick={this.ajouterUneTache}>Ajout d'une tache</button>
-        {this.state.taches.map((e) => (
-          <p>{e}</p>))}
 
+        <button onClick={this.ajouterUneTache}>Ajout d'une tache</button>
+        {this.state.taches.map((add, element) => (
+          <div>
+            <p>{add}</p>
+            <button onClick={() => this.supprimerUnetache(element)}>Supprimer</button>
+          </div>
+        ))}
       </div>
     );
   }
